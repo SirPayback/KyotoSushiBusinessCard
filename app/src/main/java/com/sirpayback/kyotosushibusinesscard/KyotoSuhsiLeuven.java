93130cc2a84d0f2beccebbbb05fec45edfa3bb41
SirPayback <mmbegames@gmail.com>
2017-01-19 13:14:26 +0100

    Initial commit

---
+++ app/src/main/java/com/sirpayback/kyotosushibusinesscard/KyotoSuhsiLeuven.java
@@ -0,0 +1,13 @@
+package com.sirpayback.kyotosushibusinesscard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KyotoSuhsiLeuven extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kyoto_suhsi_leuven);
+    }
+}
